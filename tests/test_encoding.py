25d67cfaa0d79f505d9f1f0842cb8b809dec3903
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-04-15 14:43:35 +1000

    Add set nesting tests.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -31,6 +31,7 @@ def run_test(name):
 
 def test_basic_tests():
     run_test("tests/example/booleans")
+    run_test("tests/example/nested_set")
 
 def test_basic_assignments():
     run_test("tests/example/basic_assignments")
