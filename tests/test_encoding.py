cb727f67fee8832e9c70ed79701ff63f6b21194a
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-04-18 17:29:47 +1000

    Add equaltiy, length, and concatenation for strings. Fix #28.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -32,6 +32,7 @@ def run_test(name):
 def test_basic_tests():
     run_test("tests/example/booleans")
     run_test("tests/example/nested_set")
+    run_test("tests/example/strings")
 
 
 def test_basic_assignments():
