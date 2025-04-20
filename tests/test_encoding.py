ec0b8ab72e1f759595e21cc10ad78fd9080befde
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-04-20 21:29:41 +1000

    Implement multimap equality and corresponding tests.

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -31,6 +31,7 @@ def run_test(name):
 
 def test_basic_tests():
     run_test("tests/example/booleans")
+    run_test("tests/example/multimaps")
     run_test("tests/example/nested_set")
     run_test("tests/example/strings")
 
