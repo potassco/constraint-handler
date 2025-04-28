fce92ddf2a3d1b2ee60445f793ded5a7c1f72e9b
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-04-29 00:47:24 +1000

    Update int to use compute/computed interface and add tests for ints

--- src/constraint_handler/__init__.py
+++ src/constraint_handler/__init__.py
@@ -7,13 +7,17 @@ from importlib.resources import files
 from clingo.ast import ProgramBuilder, parse_files
 from clingo.script import enable_python
 
+BOOL_LP = files("constraint_handler.data").joinpath("bool.lp")
 CONDITIONALS_LP = files("constraint_handler.data").joinpath("conditionals.lp")
 DIRECT_LP = files("constraint_handler.data").joinpath("direct.lp")
 DISPLAY_LP = files("constraint_handler.data").joinpath("display.lp")
 FLOATS_LP = files("constraint_handler.data").joinpath("floats.lp")
 GRINGO_EVAL_LP = files("constraint_handler.data").joinpath("gringoEval.lp")
-MAIN_LP = files("constraint_handler.data").joinpath("main.lp")
+INT_LP = files("constraint_handler.data").joinpath("int.lp")
 INTENSIONAL_SET_LP = files("constraint_handler.data").joinpath("intensionalSet.lp")
+MAIN_LP = files("constraint_handler.data").joinpath("main.lp")
+MULTIMAP_LP = files("constraint_handler.data").joinpath("multimap.lp")
+STRING_LP = files("constraint_handler.data").joinpath("string.lp")
 
 enable_python()
 
@@ -23,13 +27,17 @@ def add_encoding_to_program_builder(b: ProgramBuilder):
     with b:
         parse_files(
             [
+                str(BOOL_LP),
                 str(CONDITIONALS_LP),
                 str(DIRECT_LP),
                 str(DISPLAY_LP),
                 str(FLOATS_LP),
                 str(GRINGO_EVAL_LP),
-                str(MAIN_LP),
+                str(INT_LP),
                 str(INTENSIONAL_SET_LP),
+                str(MAIN_LP),
+                str(MULTIMAP_LP),
+                str(STRING_LP),
             ],
             lambda stm: b.add(stm),
         )
