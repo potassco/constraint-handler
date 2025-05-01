44d17e17313741aa84fe7d57049a42a8cbb825b1
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-05-02 01:48:59 +1000

    Rename intensionalSet.lp to set.lp and floats.lp to float.lp.

--- src/constraint_handler/__init__.py
+++ src/constraint_handler/__init__.py
@@ -11,12 +11,12 @@ BOOL_LP = files("constraint_handler.data").joinpath("bool.lp")
 CONDITIONALS_LP = files("constraint_handler.data").joinpath("conditionals.lp")
 DIRECT_LP = files("constraint_handler.data").joinpath("direct.lp")
 DISPLAY_LP = files("constraint_handler.data").joinpath("display.lp")
-FLOATS_LP = files("constraint_handler.data").joinpath("floats.lp")
+FLOAT_LP = files("constraint_handler.data").joinpath("float.lp")
 GRINGO_EVAL_LP = files("constraint_handler.data").joinpath("gringoEval.lp")
 INT_LP = files("constraint_handler.data").joinpath("int.lp")
-INTENSIONAL_SET_LP = files("constraint_handler.data").joinpath("intensionalSet.lp")
 MAIN_LP = files("constraint_handler.data").joinpath("main.lp")
 MULTIMAP_LP = files("constraint_handler.data").joinpath("multimap.lp")
+SET_LP = files("constraint_handler.data").joinpath("set.lp")
 STRING_LP = files("constraint_handler.data").joinpath("string.lp")
 
 enable_python()
@@ -31,12 +31,12 @@ def add_encoding_to_program_builder(b: ProgramBuilder):
                 str(CONDITIONALS_LP),
                 str(DIRECT_LP),
                 str(DISPLAY_LP),
-                str(FLOATS_LP),
+                str(FLOAT_LP),
                 str(GRINGO_EVAL_LP),
                 str(INT_LP),
-                str(INTENSIONAL_SET_LP),
                 str(MAIN_LP),
                 str(MULTIMAP_LP),
+                str(SET_LP),
                 str(STRING_LP),
             ],
             lambda stm: b.add(stm),
