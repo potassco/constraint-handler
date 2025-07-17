36b6a4a2e1e2dce096fddf4e3f89adff13ce6741
kstrauch94 <kstrauch0694@gmail.com>
2025-07-17 15:38:15 +0200

    some cleanup

--- src/constraint_handler/propagator.py
+++ src/constraint_handler/propagator.py
@@ -21,9 +21,8 @@ class ConstraintHandlerPropagator:
         self.assign_symbol_lit: Dict[clingo.symbol,int] = {}
         self.assign_symbol_parsed: Dict[clingo.symbol, Tuple[str, clingo.Symbol, evaluator.Expr]] = {}
 
+        # holds evaluations of assignments in the root level
         self.evaluated: Dict[clingo.symbol, Any] = {}
-
-        self.assign_dependencies: Dict[clingo.symbol, set[clingo.symbol]] = defaultdict(set)
 
         self.reasons: Dict[clingo.symbol, set[int]] = defaultdict(set)
 
@@ -33,18 +32,6 @@ class ConstraintHandlerPropagator:
     def init(self, ctl: clingo.PropagateInit):
         self.get_ensure(ctl)
         self.get_assign(ctl)
-
-        print(self.ensure_symbol_lit)
-        print(self.assign_symbol_lit)
-
-        # readable = { key : val for key,val in self.ensure_symbol_lit.items() }
-        # readable.update({ key : val for key,val in self.assign_symbol_lit.items() })
-        # print(readable)
-
-        print("Variable dependencies for assignments:")
-        for var, deps in self.assign_dependencies.items():
-            print(f"{var}: {deps}")
-        print("#"*50)
 
         print("INIT DONE")
         print("#"*50)
@@ -57,10 +44,12 @@ class ConstraintHandlerPropagator:
             print("Assignment is not total, cannot check ensures")
             return
         print(f"Assignment is total({ctl.assignment.is_total}), checking ensures")
+
         self.reasons = defaultdict(set)
         evaluations: Dict[clingo.Symbol, Any] = self.evaluated_solver_assignment(ctl)
         print(f"after evaluation, the reasons are {self.reasons}")
         self.evaluation = evaluations
+        
         backtrack = self.check_ensure(ctl, evaluations)
         print(f"CHECK DONE, backtracking {backtrack}")
 
@@ -97,44 +86,38 @@ class ConstraintHandlerPropagator:
         max_iterations: int = 10 # these 2 are just for safety in the testing phase
         iterations: int = 0
         
-        # In the future, we can do a order on which expressions to evaluate first
-        # So that we do only one pass, for now this is easier to implement
-        # Also, we can split the expressions into the ones evaluated at initialization
-        # and the other ones so that we only look at the relevant ones in this pass
+        # Each loop evaluates the assignments
+        # and updates the evaluations dictionary
         while changed and iterations < max_iterations:
             iterations += 1
             changed = False
-            print(f"Evaluating... Iteration {iterations}")
-            print(f"Current evaluations: {evaluations}")
+            print(f"Evaluating... Iteration {iterations}\nCurrent evaluations: {evaluations}")
             for symbol, lit in self.assign_symbol_lit.items():
                 name, var, expr = self.parse_assign(symbol)
                 print(f"Looking at variable {var} with expression {expr} and literal {lit} with assignment {ctl.assignment.is_true(lit)}")
                 if var in evaluations:
-                    print(f"Variable {var} already evaluated as {evaluations[var]}")
                     continue
 
                 if ctl.assignment.is_true(lit):
-                    print(f"Literal {lit} is true, evaluating expression {expr}")
-                    print(f"Evaluated dict: {evaluations}")
                     evaluated = evaluator.evaluate_expr(evaluations, expr)
                     if evaluated is not None:
+                        print(f"expression {expr} evaluated to {evaluated} for variable {var} (True literal {lit})")
                         changed = True
                         evaluations[var] = evaluated
                         self.reasons[var] = { lit }
 
                         self.reasons[var] = self.reasons[var].union(*(self.reasons[dvar] for dvar in list(evaluator.collectVars(expr))))
-                        print(f"the reason for {var} taking value {evaluated} is {self.reasons[var]}")
+                        print(f"the reason(s) for {var} taking value {evaluated} is {self.reasons[var]}")
 
-        print(f"Evaluations after {iterations} iterations: {evaluations}")
         return evaluations
     
-    def parse_assign(self, symbol: clingo.Symbol):
+    def parse_assign(self, symbol: clingo.Symbol) -> Tuple[str, clingo.Symbol, evaluator.Expr]:
         name = myClorm.cltopy(symbol.arguments[0])
         var = myClorm.cltopy(symbol.arguments[1])
         expr = myClorm.cltopy(symbol.arguments[2],evaluator.Expr)
         return name, var, expr
 
-    def parse_ensure(self, symbol: clingo.Symbol):
+    def parse_ensure(self, symbol: clingo.Symbol) -> Tuple[str, evaluator.Expr]:
         name = myClorm.cltopy(symbol.arguments[0])
         expr = myClorm.cltopy(symbol.arguments[1],evaluator.Expr)
         return name, expr
@@ -151,40 +134,12 @@ class ConstraintHandlerPropagator:
             name, var, expr = self.parse_assign(atom.symbol)
             self.assign_symbol_parsed[atom.symbol] = (name, var, expr)
 
-            self.assign_dependencies[var].update(evaluator.collectVars(expr))
-
             # Any facts assigning to a variable are stored immediately
             # in the evaluated dict
             if ctl.assignment.is_true(ctl.solver_literal(atom.literal)):
                 evaluated = evaluator.evaluate_expr(self.evaluated, expr)
                 if evaluated is not None:
                     self.evaluated[var] = evaluated
-
-    def get_base_dependencies(self, symbol: clingo.Symbol) -> set[clingo.Symbol]:
-        """
-        Returns the base dependencies of a symbol.
-        This is used to determine which variables are needed in the nogood when an ensure is violated.
-        """
-        # For now, this justs gets the variables that have a value assigned to them directly
-        # Later, we have to also check the solver literal to see if it is a fact or not.
-        # Probably, we only want to consider the variables that are NOT facts
-        # Since adding facts to the nogood does not bring any value
-
-        deps = queue.Queue()
-        for dep in self.assign_dependencies[symbol]:
-            deps.put(dep)
-
-        base_deps = set()
-        while not deps.empty():
-            dep = deps.get()
-            if len(self.assign_dependencies[dep]) > 0:
-                for sub_dep in self.assign_dependencies[dep]:
-                    deps.put(sub_dep)
-
-            else:
-                base_deps.add(dep)
-
-        return base_deps
 
     def on_model(self,model):
         self.model = []
