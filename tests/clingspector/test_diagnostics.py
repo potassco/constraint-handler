1c4336c7b7bcc0d09c0745d4a158a2da9254df3c
VictosVertex <Victos.Vertex@gmail.com>
2025-07-28 13:52:20 +0200

    rework tests, add more

--- tests/clingspector/test_diagnostics.py
+++ tests/clingspector/test_diagnostics.py
@@ -6,7 +6,7 @@ import os
 import pytest
 
 from clingspector.clingspector import Clingspector
-from clingspector.diagnostic import CyclicDependencyDiagnostic, UndefinedVariableDiagnostic
+from clingspector.diagnostic import CyclicDependencyDiagnostic, Diagnostic, UndefinedVariableDiagnostic, UnsupportedArgumentTypeDiagnostic, UnsupportedOperatorDiagnostic
 from clingspector.utils.log_formatter import LoggingFormatter
 
 logger = logging.getLogger("clingspector")
@@ -15,35 +15,36 @@ handler.setFormatter(LoggingFormatter())
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-
-@pytest.fixture
-def checker():
-    return Clingspector()
-
-
 TEST_FILES_DIR = os.path.join(os.path.dirname(__file__), "test_files")
 
 
-def test_valid(checker: Clingspector):
-    """Test a valid program that should not produce any diagnostics."""
+def get_diagnostics(file_name: str) -> list[Diagnostic]:
+    """Load a Clingspector file and return the diagnostics."""
 
-    file_path = os.path.join(TEST_FILES_DIR, "valid.lp")
-    checker.load([file_path])
-    checker.run()
-    diagnostics = checker.get_diagnostics()
+    clingspector = Clingspector()
+    file_path = os.path.join(TEST_FILES_DIR, file_name)
+    clingspector.load([file_path])
+    clingspector.run()
+    diagnostics = clingspector.get_diagnostics()
+
+    return diagnostics
+    
+
+def test_valid():
+    """Test a valid program that should not produce any diagnostics."""
+    diagnostics = get_diagnostics("valid.lp")
+
     assert len(diagnostics) == 0
 
 
-def test_undefined_variable(checker: Clingspector):
+def test_undefined_variable():
     """Test a program with an undefined variable.
 
     This should produce a diagnostic indicating the undefined variable.
     """
 
-    file_path = os.path.join(TEST_FILES_DIR, "undefined_variable.lp")
-    checker.load([file_path])
-    checker.run()
-    diagnostics = checker.get_diagnostics()
+    diagnostics = get_diagnostics("undefined_variable.lp")
+
     assert len(diagnostics) == 1
     diag = diagnostics[0]
     assert isinstance(diag, UndefinedVariableDiagnostic)
@@ -51,50 +52,71 @@ def test_undefined_variable(checker: Clingspector):
     assert diag.undefined_variable == "y"
 
 
-def test_direct_cycle(checker: Clingspector):
+def test_direct_cycle():
     """Test a program with a direct cyclic dependency.
 
     This should produce a diagnostic indicating the cyclic dependency.
     """
 
-    file_path = os.path.join(TEST_FILES_DIR, "direct_cycle.lp")
-    checker.load([file_path])
-    checker.run()
-    diagnostics = checker.get_diagnostics()
+    diagnostics = get_diagnostics("direct_cycle.lp")
+
     assert len(diagnostics) == 1
     diag = diagnostics[0]
     assert isinstance(diag, CyclicDependencyDiagnostic)
     assert diag.involved_variables == ["x"]
 
 
-def test_simple_cycle(checker: Clingspector):
+def test_simple_cycle():
     """Test a program with a simple cyclic dependency.
 
     This should produce a diagnostic indicating the cyclic dependency.
     """
 
-    file_path = os.path.join(TEST_FILES_DIR, "simple_cycle.lp")
-    checker.load([file_path])
-    checker.run()
-    diagnostics = checker.get_diagnostics()
+    diagnostics = get_diagnostics("simple_cycle.lp")
+
     assert len(diagnostics) == 1
     diag = diagnostics[0]
     assert isinstance(diag, CyclicDependencyDiagnostic)
     assert diag.involved_variables == ["x", "y"]
 
 
-def test_overlapping_cycles(checker: Clingspector):
+def test_overlapping_cycles():
     """Test a program with overlapping cycles.
 
     This should still only produce a single diagnostic
     containing all involved variables.
     """
 
-    file_path = os.path.join(TEST_FILES_DIR, "overlapping_cycle.lp")
-    checker.load([file_path])
-    checker.run()
-    diagnostics = checker.get_diagnostics()
+    diagnostics = get_diagnostics("overlapping_cycle.lp")
+
     assert len(diagnostics) == 1
     diag = diagnostics[0]
     assert isinstance(diag, CyclicDependencyDiagnostic)
     assert diag.involved_variables == ["a", "b", "c", "d", "e"]
+
+def test_unsupported_operator():
+    """Test a program with an unsupported operator.
+
+    This should produce a diagnostic indicating the unsupported operator.
+    """
+
+    diagnostics = get_diagnostics("unsupported_operator_type.lp")
+
+    assert len(diagnostics) == 1
+    diag = diagnostics[0]
+    assert isinstance(diag, UnsupportedOperatorDiagnostic)
+    assert diag.operator == "nonsense"
+
+def test_unsupported_argument_type():
+    """Test a program with an unsupported argument type.
+
+    This should produce a diagnostic indicating the unsupported argument type.
+    """
+
+    diagnostics = get_diagnostics("unsupported_argument_type.lp")
+
+    assert len(diagnostics) == 1
+    diag = diagnostics[0]
+    assert isinstance(diag, UnsupportedArgumentTypeDiagnostic)
+    assert diag.operator == "add"
+    assert diag.arguments == ["int", "float"]
