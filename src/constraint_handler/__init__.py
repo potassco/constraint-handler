f00d315d6f2631b19a5686a25c0ec13fcad06a1b
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2025-08-09 16:56:01 +1000

    delete display.lp + group python helper functions in a single file.

--- src/constraint_handler/__init__.py
+++ src/constraint_handler/__init__.py
@@ -10,12 +10,12 @@ from clingo.script import enable_python
 BOOL_LP = files("constraint_handler.data").joinpath("bool.lp")
 CONDITIONALS_LP = files("constraint_handler.data").joinpath("conditionals.lp")
 DIRECT_LP = files("constraint_handler.data").joinpath("direct.lp")
-DISPLAY_LP = files("constraint_handler.data").joinpath("display.lp")
 FLOAT_LP = files("constraint_handler.data").joinpath("float.lp")
 GRINGO_EVAL_LP = files("constraint_handler.data").joinpath("gringoEval.lp")
 INT_LP = files("constraint_handler.data").joinpath("int.lp")
 MAIN_LP = files("constraint_handler.data").joinpath("main.lp")
 MULTIMAP_LP = files("constraint_handler.data").joinpath("multimap.lp")
+PYTHON_HELPER_LP = files("constraint_handler.data").joinpath("pythonHelper.lp")
 SET_LP = files("constraint_handler.data").joinpath("set.lp")
 STRING_LP = files("constraint_handler.data").joinpath("string.lp")
 SYMBOL_LP = files("constraint_handler.data").joinpath("symbol.lp")
@@ -31,12 +31,12 @@ def add_encoding_to_program_builder(b: ProgramBuilder):
                 str(BOOL_LP),
                 str(CONDITIONALS_LP),
                 str(DIRECT_LP),
-                str(DISPLAY_LP),
                 str(FLOAT_LP),
                 str(GRINGO_EVAL_LP),
                 str(INT_LP),
                 str(MAIN_LP),
                 str(MULTIMAP_LP),
+                str(PYTHON_HELPER_LP),
                 str(SET_LP),
                 str(STRING_LP),
                 str(SYMBOL_LP),
