aafabac7ba7e53d2f9078053d23b228291ef1fd9
Klaus Strauch <kstrauch0694@gmail.com>
2025-11-14 14:22:50 +0100

    refactor prop into multiple files

---
+++ src/constraint_handler/PropagatorConstants.py
@@ -0,0 +1,23 @@
+import enum
+from dataclasses import dataclass
+
+DEBUG_PRINT = False
+
+FALSE_ASSIGNMENTS = "FALSE_ASSIGNMENTS"
+# enum for value_not_set, assignment_is_false, and value_is_none
+class ValueStatus(enum.Enum):
+    NOT_SET = "value_not_set"
+    ASSIGNMENT_IS_FALSE = "assignment_is_false"
+
+
+@dataclass
+class AtomNames:
+    ASSIGN: str = "propagator_assign"
+    ENSURE: str = "propagator_ensure"
+    EVALUATE: str = "evaluate"
+    SET_DECLARE: str = "propagator_set_declare"
+    SET_ASSIGN: str = "propagator_set_assign"
+    MULTIMAP_DECLARE: str = "propagator_multimap_declare"
+    MULTIMAP_ASSIGN: str = "propagator_multimap_assign"
+    SOLVER_ID: str = "_main_solverIdentifier"
+    OPTIMIZE_SUM: str = "optimize_maximizeSum"
