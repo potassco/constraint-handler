77c026a6aeafb0db1643fdb66ed138ce507e23b0
Klaus Strauch <kstrauch0694@gmail.com>
2025-11-19 13:17:15 +0100

    strings are supported

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -102,7 +102,6 @@ def test_engine_propagator():
         "optimize_ints",
         "set_iterations",
         "set_selfref",
-        #"strings",
         "variables",
     ]
     for test in base_tests + extra:
