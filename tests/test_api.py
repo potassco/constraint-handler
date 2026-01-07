59cdeca9620ced4f51585ca319e9c536fb90791b
Abdallah Saffidine <abdallah.saffidine@gmail.com>
2026-01-07 22:53:10 +1100

    Change value/3 to value/2, set_value/3 to set_value/2, and multimap_value/5 to multimap_value/3.

--- tests/test_api.py
+++ tests/test_api.py
@@ -18,14 +18,14 @@ def test_add_ctrl():
         """
     assign(assign_x,x,val(int,20)).
     assign(assign_y,y,operation(add,(variable(x),(val(int,10),())))).
-    #show value/3.
+    #show value/2.
     """
     )
     ctrl.ground()
     solve_handle = ctrl.solve(yield_=True)
     for model in solve_handle:
         solution = {fact.__str__() for fact in model.symbols(shown=True, theory=True)}
-        assert solution == {"value(x,int,20)", "value(y,int,30)"}
+        assert solution == {"value(x,val(int,20))", "value(y,val(int,30))"}
 
 
 def get_solutions(program: str, use_prop=False) -> Iterator[Set[Symbol]]:
@@ -59,13 +59,13 @@ def test_prop():
     constraint_expr = """
     assign(assign_bike_frame_size, bike_frame_size, val(int,26)).
     assign(assign_bike_frame_type, bike_frame_type, operation(ite, (operation(eq, (variable(bike_frame_size), (val(int,26), ()))), (val(str,"Mountain"), (val(str,"Road"), ()))))).
-    #show value/3.
+    #show value/2.
     """
 
     for solution in get_solutions(constraint_expr, True):
         assert solution == {
-            "value(bike_frame_size,int,26)",
-            'value(bike_frame_type,str,"Mountain")',
+            "value(bike_frame_size,val(int,26))",
+            'value(bike_frame_type,val(str,"Mountain"))',
         }
 
 
@@ -73,13 +73,13 @@ def test_noprop():
     constraint_expr = """
     assign(assign_bike_frame_size, bike_frame_size, val(int,26)).
     assign(assign_bike_frame_type, bike_frame_type, operation(ite, (operation(eq, (variable(bike_frame_size), (val(int,26), ()))), (val(str,"Mountain"), (val(str,"Road"), ()))))).
-    #show value/3.
+    #show value/2.
     """
 
     for solution in get_solutions(constraint_expr):
         assert solution == {
-            "value(bike_frame_size,int,26)",
-            'value(bike_frame_type,str,"Mountain")',
+            "value(bike_frame_size,val(int,26))",
+            'value(bike_frame_type,val(str,"Mountain"))',
         }
 
 
@@ -87,7 +87,7 @@ def test_add():
     constraint_expr = """
     assign(assign_x, x, val(int,20)).
     assign(assign_y, y, operation(add, (variable(x), (val(int,10), ())))).
-    #show value/3.
+    #show value/2.
     """
 
     ctrl = Control("0")
@@ -103,4 +103,4 @@ def test_add():
         for fact in model.symbols(shown=True):
             solution.add(fact.__str__())
 
-        assert solution == {"value(x,int,20)", "value(y,int,30)"}
+        assert solution == {"value(x,val(int,20))", "value(y,val(int,30))"}
