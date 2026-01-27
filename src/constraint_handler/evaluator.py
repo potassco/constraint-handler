4e87417043d80973b537051e85e4004d339c4ac1
VictosVertex <Victos.Vertex@gmail.com>
2026-01-27 13:07:09 +0100

    let strings use the add operator

--- src/constraint_handler/evaluator.py
+++ src/constraint_handler/evaluator.py
@@ -313,6 +313,10 @@ class Evaluator:
                 }
 
                 if o in foldable:
+                    # This makes it possible to use `add` for strings.
+                    if len(args) > 0 and isinstance(args[0], str) and o == BinaryOperator.add:
+                        return "".join(args)
+
                     return foldable[o](args)
 
                 if len(args) == 1:
