e20e57ae6ed5c612202a56e583baddfe50a1a991
Max Ostrowski <max.ostrowski@potassco.com>
2026-04-15 13:54:54 +0200

    add failed example for invalid warning

--- tests/test_encoding.py
+++ tests/test_encoding.py
@@ -65,6 +65,7 @@ base_tests = [
     "set_manipulations",
     "set_selfref",
     "strings",
+    "statement_python_declared_output",
     "sum_aggregates",
     "sum_chain_performance",
     "type_checking",
