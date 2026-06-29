819dcdecb3844b5dcb6114983d10a7746fd8ae91
David Rajaratnam <daver@gemarex.com.au>
2026-06-29 11:28:30 +1000

    Remove some grounding non-determinism (#325)
    * Removed some non-determinism by sorting the output of @-functions
    
    * Need to sort before running enumerate()
    
    * Make myClorm's outputting of sets deterministic
    
    Sets are encoded using a set operator combined with a list of elements. To make the output of
    myClorm's encoding deterministic this list is sorted.
    
    * Undo sorted of pythonEvalExpr() - I think it is already deterministic
    
    * Unit test for python externals interface
    
    Currently tests for cases where the output should be sorted

---
+++ tests/test_python_externals_interface.py
@@ -0,0 +1,59 @@
+from __future__ import annotations
+
+import clingo
+
+import constraint_handler.myClorm as myClorm
+import constraint_handler.python_externals.interface as interface
+import constraint_handler.schemas.expression as expression
+
+
+def _clear_interface_caches() -> None:
+    interface.pythonExpressionVariable.cache_clear()
+    interface.pythonEnumerateVariables.cache_clear()
+    interface.pythonEvalExpr.cache_clear()
+    interface.pythonStatementVariables.cache_clear()
+
+
+def test_python_expression_variable_returns_sorted_symbols():
+    _clear_interface_caches()
+
+    expr = (expression.Variable("b"), expression.Variable("a"))
+    cl_expr = myClorm.pytocl(expr)
+
+    result = interface.pythonExpressionVariable(cl_expr)
+
+    assert result == [clingo.String("a"), clingo.String("b")]
+
+
+def test_python_enumerate_variables_sorts_enumerated_symbols():
+    _clear_interface_caches()
+
+    expr = (expression.Variable("b"), expression.Variable("a"))
+    cl_expr = myClorm.pytocl(expr)
+
+    result = interface.pythonEnumerateVariables(cl_expr)
+
+    expected_a_then_b = [
+        myClorm.pytocl((0, "a")),
+        myClorm.pytocl((1, "b")),
+        clingo.Function("length", [clingo.Number(2)]),
+    ]
+    assert result == expected_a_then_b
+
+
+def test_python_statement_variables_returns_sorted_symbols():
+    _clear_interface_caches()
+
+    cl_code = myClorm.pytocl("b = 1.0\na = 1")
+    cl_in_types = myClorm.pytocl([])
+    cl_id = myClorm.pytocl([])
+
+    result = interface.pythonStatementVariables(cl_code, cl_in_types, cl_id)
+
+    expected = sorted(
+        [
+            myClorm.pytocl(("a", interface.type_.BaseType.int)),
+            myClorm.pytocl(("b", interface.type_.BaseType.float)),
+        ]
+    )
+    assert result == expected
