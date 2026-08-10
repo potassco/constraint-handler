e108830f13f9b1f2931396a61d7b58c28e9cffd3
Klaus Strauch <kstrauch0694@gmail.com>
2026-08-10 11:30:21 +0200

    Update propagator check only activation

--- tests/test_performance.py
+++ tests/test_performance.py
@@ -42,8 +42,10 @@ def run_benchmark_program(benchmark_case: PerformanceBenchmark) -> None:
             benchmark_options.extend(["-c", f"{name}={value}"])
 
     ctl = clingo.Control(benchmark_options)
-    constraint_handler.add_to_control(ctl, propagator_check_only=benchmark_case.check_mode)
+    constraint_handler.add_to_control(ctl)
     ctl.add(f"defaultEngine({benchmark_case.engine}).")
+    if benchmark_case.engine == "propagator" and benchmark_case.check_mode:
+        ctl.add("propagator_check_only.")
     ctl.load(os.fspath(benchmark_case.program_path))
     ctl.ground()
     ctl.solve()
